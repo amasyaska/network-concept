838441036a06db1dbf4ba9842c4f54f94535e089
amasyaska <bogdan.kolesnikov12345@gmail.com>
2024-04-08 19:29:22 +0300

    according to ideas 8 and 9, added public method Hub::request_to_send_message_from_client_to_server, and protected method Hub::send_message_from_client_to_server updated documentation

--- src/hub.hpp
+++ src/hub.hpp
@@ -8,16 +8,16 @@ class Hub
 	/*
 	 * this class is a container for nodes
 	 */
-private:
-	int send_message_to_node(unsigned long long const &address, std::string const &message); // only for use of Nodes as a friend method
 protected:		// not private for HubDebug class
 	// hashtable field (we can use vector, but how will we be founding element by index, I suggest to use hashtable (object -> address) instead) (STL doesn't have hashtable implemented bruh)
 	std::vector< std::pair<const Node *, unsigned long long> > container;		// pair: node pointer to address (uint64_t)
 	unsigned long long address_top;						// when we add new node, we add to address_top
+	int send_message_from_client_to_server(NodeClient * const client, NodeServer * const server);	// for internal use ONLY
 public:
 	Hub(void);
 	~Hub(void);
 
 	int add_node(Node * const ptr_node);
-	int remove_node(Node * const ptr_node);	
+	int remove_node(Node * const ptr_node);
+	int request_to_send_message_from_client_to_server(NodeClient * const client, NodeServer * const server); // for external use
 };
--- src/node_client.hpp
+++ src/node_client.hpp
@@ -1,4 +1,5 @@
 class NodeClient : public Node
 {
-
+public:
+	
 }
